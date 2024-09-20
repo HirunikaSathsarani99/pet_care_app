19805846985adca0d51ffd6c6ebceb092f470cbe
rashmirekha99 <kothalawalarr.19@uom.lk>
2024-09-21 00:19:43 +0530

    added android firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pet_care_app/view/automatic_cleaning_page.dart';
 import 'package:pet_care_app/view/food_level_page.dart';
@@ -8,8 +9,19 @@ import 'package:pet_care_app/view/registration_screen.dart';
 import 'package:pet_care_app/view/spalsh_screen.dart';
 import 'package:pet_care_app/view/temperature_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+    apiKey: 'AIzaSyDpmwPSMUqrzzz-Oe4GC6UW-JEzwgtMpd4',
+    appId: '1:535515887435:android:f240e863cd3cf9703c5ace',
+    messagingSenderId: '535515887435',
+    projectId: 'pet-care-app-2634d',
+    storageBucket: 'pet-care-app-2634d.appspot.com',
+  ));
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -21,21 +33,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pet Care',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home:  SplashScreen(),
-       routes: {
-      '/register': (context) => RegisterScreen(), 
-      '/login': (context) => LoginScreen(), 
-      '/home': (context) => HomeScreen(), 
-      '/automatic Cleaning': (context) => AutomaticCleaningPage(), 
-      '/light sheduling': (context) => LightSchedulingPage(), 
-      '/temerature': (context) => TemperatureHumidityPage(), 
-      '/food level': (context) => FoodLevelPage(),
-    },
+      home: SplashScreen(),
+      routes: {
+        '/register': (context) => RegisterScreen(),
+        '/login': (context) => LoginScreen(),
+        '/home': (context) => HomeScreen(),
+        '/automatic Cleaning': (context) => AutomaticCleaningPage(),
+        '/light sheduling': (context) => LightSchedulingPage(),
+        '/temerature': (context) => TemperatureHumidityPage(),
+        '/food level': (context) => FoodLevelPage(),
+      },
     );
   }
 }
